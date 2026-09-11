dc4e0bc223f8e5cd2aa86ba205b95be1839a239c
tostderr2 <tostderr@gmail.com>
2026-09-11 17:15:17 +0530

    Fixed unit step, negative ray dir was the problem

--- src/core/raycaster/raycaster.cpp
+++ src/core/raycaster/raycaster.cpp
@@ -22,8 +22,8 @@ RcHit castRay(float lookAngle, float rayAngle, float originX, float originY, con
     // unit vector triangle vs a triangle of the actual distance ray has to travel
     // both are drawn on top so same angle
     // serach what is a "big number" in cpp. it was a exponent
-    Vec2f rayUnitStepSize = {(rayDir.x == 0.0f) ? 100 : 1.0f / rayDir.x,
-                             (rayDir.y == 0.00f) ? 100 : 1.0f / rayDir.y};
+    Vec2f rayUnitStepSize = {(rayDir.x == 0.0f) ? 999999 : std::abs(1.0f / rayDir.x),
+                             (rayDir.y == 0.00f) ? 999999 : std::abs(1.0f / rayDir.y)};
 
     // initial distance to nearest  grid boundary
     Vec2f sideDist;
@@ -70,14 +70,14 @@ RcHit castRay(float lookAngle, float rayAngle, float originX, float originY, con
     int hitCellType = 0;
 
     if (hit) {
-		// which wall was hit
-		hitCellType = map.cells[mapPos.y * map.width + mapPos.x];
-		// get per dist
+        // which wall was hit
+        hitCellType = map.cells[mapPos.y * map.width + mapPos.x];
+        // get per dist
         if (side) {
-            perpWallDist = sideDist.y- rayUnitStepSize.y;
+            perpWallDist = sideDist.y - rayUnitStepSize.y;
         } else {
 
-            perpWallDist = sideDist.x- rayUnitStepSize.x;
+            perpWallDist = sideDist.x - rayUnitStepSize.x;
         }
         perpWallDist *= cosf(rayAngle - lookAngle);
     }
